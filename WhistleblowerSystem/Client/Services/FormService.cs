7d66d63f16dba4630d45a0c3caa86d075cd2335b
sarazarubica98 <sara.zarubica@students.fhnw.ch>
2021-12-11 12:55:25 +0100

    Vorbereitung API

--- WhistleblowerSystem/Client/Services/FormService.cs
+++ WhistleblowerSystem/Client/Services/FormService.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using WhistleblowerSystem.Client.Utils;
 using WhistleblowerSystem.Shared.DTOs;
 using WhistleblowerSystem.Shared.Enums;
 using WhistleblowerSystem.Shared.Models;
@@ -14,10 +15,12 @@ namespace WhistleblowerSystem.Client.Services
         private FormDto? _currentForm;
         private FormModel? _currentFormModel;
         private List<FormDto>? _allForms;
+        //private ReportApi _reportApi;
 
         public FormService(HttpClient http)
         {
             _http = http;
+            //_reportApi = new ReportApi();
         }
 
         public async Task<FormDto?> GetForm()
@@ -31,6 +34,7 @@ namespace WhistleblowerSystem.Client.Services
         }
 
         public async Task<FormDto?> LoadById(string id) {
+            //_reportApi.ReportsgetReportbyId(id);
             HttpResponseMessage? response = await _http.GetAsync($"Form/{id}");
             if (!string.IsNullOrEmpty(value: await response.Content.ReadAsStringAsync()))
             {
@@ -41,6 +45,7 @@ namespace WhistleblowerSystem.Client.Services
         
         public async Task <List<FormDto>?> LoadAll()
         {
+            //_reportApi.ReportsgetReports();
             HttpResponseMessage? response = await _http.GetAsync("Form/getAll");
             if (!string.IsNullOrEmpty(value: await response.Content.ReadAsStringAsync()))
             { 
@@ -51,21 +56,41 @@ namespace WhistleblowerSystem.Client.Services
 
         public async Task<FormDto?> Save(FormDto formDto)
         {
+            //var titleField = getField(formDto.FormFields, "Beschreibung");
+            //var title = (titleField != null && titleField.SelectedValues.Count > 0) ? titleField.SelectedValues[0] : string.Empty;
+            //var descriptionField = getField(formDto.FormFields, "Vorfall");
+            //var description = (descriptionField != null && descriptionField.SelectedValues.Count > 0) ? descriptionField.SelectedValues[0] : string.Empty;
+            //ReportModel reportModel = new ReportModel();
+            //reportModel.Title = title;
+            //reportModel.Description = description;
+
+            //_reportApi.ReportspostReport(reportModel);
             HttpResponseMessage? response = await _http.PostAsJsonAsync("Form/save", formDto);
             if (!string.IsNullOrEmpty(value: await response.Content.ReadAsStringAsync()))
             {
-                _currentForm = await response.Content.ReadFromJsonAsync<FormDto>();
+                _currentForm = await     response.Content.ReadFromJsonAsync<FormDto>();
             }
             return _currentForm;
         }
         
         public async Task AddMessage(string formId, FormMessageDto messageDto)
-        { 
+        {
+            //RoleEnumModel role = messageDto.User == null ? RoleEnumModel.User : RoleEnumModel.Staff;
+            //var messageModel = new MessageModel(messageDto.Text);
+            //var messageObjectModel = new MessageObjectModel(role, messageModel);
+            //_reportApi.ReportsAddMessageByReportId(formId, messageObjectModel);
+
             await _http.PostAsJsonAsync($"Form/{formId}/addMessage", messageDto);
         }
-        
+
+        public async Task AddFile(string formId, AttachementMetaDataDto attachementMetaDataDto)
+        {
+            await _http.PostAsJsonAsync($"Form/{formId}/addFile", attachementMetaDataDto);
+        }
+
         public async Task UpdateState(string id, ViolationState state)
         {
+            //_reportApi.ReportsUpdateStatusByReportId(id, GetStatusModelByState(state));
             await _http.PostAsJsonAsync($"Form/{id}/changeState/{state}", new object());
         }
 
@@ -124,6 +149,21 @@ namespace WhistleblowerSystem.Client.Services
             }
             return optionalFields;
         }
+
+        //private string GetStatusModelByState(ViolationState state) {
+        //    switch (state) {
+        //        case ViolationState.Undefined:
+        //            return StatusModel.Undefined.ToString();
+        //        case ViolationState.Accepted:
+        //            return StatusModel.Received.ToString();
+        //        case ViolationState.InProgress:
+        //            return StatusModel.Processing.ToString();
+        //        case ViolationState.Done:
+        //            return StatusModel.Closed.ToString();
+        //        default:
+        //            return StatusModel.Undefined.ToString();
+        //    }
+        //}
 
     }
 }
