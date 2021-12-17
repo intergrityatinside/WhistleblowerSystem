5ed80e47648d8bd430679e0b0af6ee11af74ffa8
sarazarubica98 <sara.zarubica@students.fhnw.ch>
2021-12-17 11:05:54 +0100

    Bilder in Anleitung hinzugefügt

---
+++ WhistleblowerSystem/Client/Pages/Manual.razor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhistleblowerSystem.Client.Services;
+using WhistleblowerSystem.Shared.Enums;
+
+namespace WhistleblowerSystem.Client.Pages
+{
+}
